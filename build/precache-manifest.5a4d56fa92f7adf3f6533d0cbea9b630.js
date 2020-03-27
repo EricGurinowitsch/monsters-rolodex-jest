f822cadef10cb705b727aeda61392c1bf4c02059
Eric Gurinowitsch <egurinowitsch@Erics-MacBook-Pro-2.local>
2020-03-27 16:32:59 -0400

    first commit

---
+++ build/precache-manifest.5a4d56fa92f7adf3f6533d0cbea9b630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da40b40c03a7d105e2e1afc9741f02ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4ccdc8e0a6e6631045b0",
+    "url": "/monsters-rolodex/static/css/main.b053097d.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ccdc8e0a6e6631045b0",
+    "url": "/monsters-rolodex/static/js/main.6d08ae3f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
